751300593734072d9523c24701274ee60b1c6bdf
skudfarkas <62894514+skudfarkas@users.noreply.github.com>
2020-04-23 19:02:08 -0400

    Update AppointmentSlot.h

--- src/AppointmentSlot.h
+++ src/AppointmentSlot.h
@@ -23,6 +23,8 @@ class AppointmentSlot{
     private: // Attributes
         DateTime startTime;   ///< The Start Time of the appointment
         DateTime endTime;     ///< The end time of the appointment
+        int refNum;	  ///< reference number for the slot
+	    bool filled;	///< Whether the slot is filled or not with an appointment
     
     public:
         // Accessors
@@ -36,6 +38,16 @@ class AppointmentSlot{
          * @return DateTime the ending time of the AppointmentSlot
          */
         DateTime getEndTime();
+    
+        /** @brief Retrieve the reference number of the slot
+         * @return int the reference number of the slot
+         */
+    	int getRefNum();
+	    
+        /** @brief Retrieve whether the slot is filled or not
+         * @return bool whether the slot is filled or not
+         */
+        bool getFilled();
 
         // Modifiers
 
@@ -50,6 +62,18 @@ class AppointmentSlot{
          * @return none
          */
         void setEndTime(DateTime endTime);
+    
+        /** @brief Set the refernece number of the slot
+         * @param int the reference number of the slot
+         * @return none
+         */
+    	void setRefNum(int num);
+    
+        /** @brief Set whether the slot is filled or not
+         * @param int 1 if the slot is filled, 0 if it is empty
+         * @return none
+         */
+	    void setFilled(int i);
 };
 
 #endif
