b2c4c6b3011cdc22cce45f896214599233f8eb4f
skudfarkas <62894514+skudfarkas@users.noreply.github.com>
2020-04-23 11:44:44 -0400

    Update Personnel.h

--- src/Personnel.h
+++ src/Personnel.h
@@ -1,85 +1,81 @@
-/**
- * @class Personnel
- * 
- * @brief Defines the object of an employee of the KSU health services
- * that provides care
- * 
- * @author Alex Sweeney
- * 
- * @version 1.0
- * 
- * Created on Sun Mar 29 2020
- */
-
 #ifndef PERSONNEL_H
 #define PERSONNEL_H
 
 #include <string>
 #include "AppointmentSlot.h"
+#include<vector>
 
 using namespace std;
 
 class Personnel {
-    private: // attributes
-        string name;                   ///< The name of the personnel
-        string type;                   ///< The type of the peronnel: Nurse or Counselor
-        int employeeID;                ///< The employeeID of the personnel
-        AppointmentSlot *workingTimes; ///< Dynamic array that defines their schedule
+private: ///< attributes
+	string name;                   ///< The name of the personnel
+	string type;                   ///< The type of the peronnel: Nurse or Counselor
+	int employeeID;                ///< The employeeID of the personnel
+	vector<AppointmentSlot> workingTimes; ///< Dynamic array that defines their schedule...also changed this to vector
+	///<friend ostream& operator<<(ostream& os, const Personnel& a);
+public:
+	///<constructors
+	Personnel();
+	Personnel(string n, string t, int id, vector<AppointmentSlot> w);
 
-    public:
-        // Accessors
+	///< Accessors
+	/** @brief Retrieve the name of the personnel
+	* @return String the name of the personnel
+	*/
+	string getName();
 
-        /** @brief Retrieve the name of the personnel
-         * @return string The name of the personnel
-         */
-        string getName();
+	/** @brief Retrieve the type of personnel
+	* @return String the type of personnel
+	*/
+	string getType();
 
-        /** @brief Retrieve the type of the personnel
-         * @return string The type of the personnel
-         */
-        string getType();
+	/** @brief Retrieve the ID of the personnel
+	* @return Int The ID of the personnel
+	*/
+	int getEmployeeId();
 
-        /** @brief Retrieve the id of the personnel
-         * @return int The id of the personnel
-         */        
-        int getEmployeeId();
+	/** @brief Retrieve a vector of working times of the personnel
+	* @return Vector<AppointmentSlot> a vector of working times of the personnel
+	*/
+	vector<AppointmentSlot> getWorkingTimes(); ///<changed this to vector
 
-        /** @brief Retrieve the schedule of the personnel
-         * @return AppointmentSlot* The schedule of the personnel
-         */        
-        AppointmentSlot* getWorkingTimes();
+	/** @brief Retrieve the AppointmentSlot that coincides with the reference number
+	* @param Int The reference number of the appointment slot
+	* @return AppointmentSlot The AppointmentSlot that is referenced to
+	*/
+	AppointmentSlot getAppointmentSlot(int ref);
+	
+	///<modifiers
+	/** @brief Set the Type of personnel
+	* @param String The type of the personnel
+	* @return None
+	*/
+	void setType(string t);
 
-        // Modifiers
+	/** @brief Set the Name of the personnel
+	* @param String The name of the personnel
+	* @return None
+	*/
+	void setName(string n);
 
-        /** @brief Set the type of the personnel
-         * @param type The type of the personnel
-         * @return none
-         */        
-        void setType(string type);
+	/** @brief set the ID of the personnel
+	* @param Int the ID number of the personnel
+	* @return None
+	*/
+	void setEmployeeId(int id);
 
-        /** @brief Set the name of the personnel
-         * @param name The name of the personnel
-         * @return none
-         */        
-        void setName(string name);
+	/** @brief Add an appointment slot to the working times of the personnel
+	* @param AppointmentSlot The appointment slot that will be added to the working times of the personnel
+	* @return None
+	*/
+	void addWorkingTime(AppointmentSlot w);
 
-        /** @brief Set the id of the personnel
-         * @param employeeID The id of the personnel
-         * @return none
-         */
-        void setEmployeeId(int employeeID);
-
-        /** @brief Add a working time of the personnel
-         * @param workTime The working time of the personnel
-         * @return none
-         */
-        void addWorkingTime(AppointmentSlot workTime);
-
-        /** @brief Remove a working time of the personnel
-         * @param workTime The working time of the personnel
-         * @return none
-         */
-        void removeWorkingTime(AppointmentSlot workTime)
+	/** @brief Remove an appointment slot from the working times of the personnel
+	* @param AppointmentSlot The appointment slot that will be removed from the working times of the personnel
+	* @return None
+	*/
+	void removeWorkingTime(AppointmentSlot w);
 
 };
 
