d7cc88e1addd9aaea5a92387f0478283072befbb
skudfarkas <62894514+skudfarkas@users.noreply.github.com>
2020-04-23 11:37:09 -0400

    Update Invoice.h

--- src/Invoice.h
+++ src/Invoice.h
@@ -1,7 +1,7 @@
 /**
 * @class Invoice
 * 
-* @brief this class generates the invoices that are sent out to the patients.
+* this class generates the invoices that are sent out to the patients.
 * it also tracks whether the invoice is paid or not and what time it was created.
 * 
 * @author Scott Caldwell
@@ -17,82 +17,74 @@
 #include <string>
 #include "datetime.h"
 #include "Visit.h"
-#include "Payment.h"
+
+
 
 using namespace std;
 
 class Invoice {
-	private:
-		//Attributes
-		Visit visit;	///< The visit that the invoice refers to
-		int cost;	///< The cost of the invoice
-		DateTime dateCreated;	///< The time the invoice was created
-		bool paid;	///< Whether the invoice is paid or not
-	public:
-		//Constructors
-		/**
-		 * @brief Construct an invoice object
-		 */
-		Invoice();
+private:
+	//Attributes
+	Visit visit;	///< The visit that the invoice refers to
+	int cost;	///< The cost of the invoice
+	int amountPaid; ///< The amount paid towards invoice
+	DateTime dateCreated;	///< The time the invoice was created
+	int refNum;	///< The reference number for the invoice
+	bool paid;	///< Whether the invoice is paid or not
+public:
+	//Constructors
+	Invoice();
+	Invoice(Visit visit, int cost, DateTime dateCreated, bool paid);
+	
+	//Destructors
+	~Invoice();
 
-		/**
-		 * @brief Construct an invoice object
-		 * @param visit The viist the invoice is associated with
-		 * @param cost The balance on the invoice
-		 * @param dateCreated The date and time the invoice was created
-		 * @param paid Whether or not it has been paid yet
-		 */
-		Invoice(Visit visit, int cost, DateTime dateCreated, bool paid);
-		
-		//Destructors
-		~Invoice();
+	//Accessors
 
-		//Accessors
+	/** @brief Retrieve the visit that corresponds with the invoice
+	* @return Visit that corresponds with the invoice
+	*/
+	Visit getVisit();
 
-		/** @brief Retrieve the visit that corresponds with the invoice
-		* @return Visit that corresponds with the invoice
-		*/
-		Visit getVisit();
+	/** @brief Retrieve the cost of the invoice
+	* @return Int the cost of the invoice
+	*/
+	int getCost();
 
-		/** @brief Retrieve the cost of the invoice
-		* @return Int the cost of the invoice
-		*/
-		int getCost();
+	/** @brief Retrieve the time the invoice was created
+	* @return DateTime the time the invoice was created
+	*/
+	DateTime getDateCreated();
 
-		/** @brief Retrieve the time the invoice was created
-		* @return DateTime the time the invoice was created
-		*/
-		DateTime getDateCreated();
+	/** @brief Retrieve if the invoice is paid
+	* @return Bool if the invoice is paid
+	*/
+	bool isPaid();
 
-		/** @brief Retrieve if the invoice is paid
-		* @return Bool if the invoice is paid
-		*/
-		bool isPaid();
+	//Modifiers
 
-		//Modifiers
+	/** @brief Set the visit of the invoice
+	* @param Visit the visit that corresponds with the invoice
+	* @return None
+	*/
+	void setVisit(Visit v);
 
-		/** @brief Set the visit of the invoice
-		* @param visit the visit that corresponds with the invoice
-		* @return None
-		*/
-		void setVisit(Visit visit);
+	/** @brief Set the cost of the invoice
+	* @param Int the cost of the invoice
+	* @return None
+	*/
+	void setCost(int c);
 
-		/** @brief Set the cost of the invoice
-		* @param cost the cost of the invoice
-		* @return None
-		*/
-		void setCost(int cost);
+	/** @brief Set the date the invoice was created
+	* @param DateTime the date the invoice was created
+	* @return None
+	*/
+	void setDateCreated(DateTime d);
 
-		/** @brief Set the date the invoice was created
-		* @param dateCreated the date the invoice was created
-		* @return None
-		*/
-		void setDateCreated(DateTime dateCreated);
-
-		/** @brief pay the invoice
-		* @param payment the amount paid toward the invoice
-		* @return None
-		*/
-		void pay(Payment& payment);
+	/** @brief pay the invoice
+	* @param Int the amount paid toward the invoice
+	* @return None
+	*/
+	void pay(int pay);
 };
 #endif
