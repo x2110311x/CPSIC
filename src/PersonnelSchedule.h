3e1e79322287adee43822dc201e9b65732945cef
skudfarkas <62894514+skudfarkas@users.noreply.github.com>
2020-04-23 11:47:12 -0400

    Update PersonnelSchedule.h

--- src/PersonnelSchedule.h
+++ src/PersonnelSchedule.h
@@ -1,11 +1,11 @@
 /**
- * @file PersonnelSchedule.h
- * @author Adam Adkins
- * @brief Defines the daily schedule of appointments for a specific Personnel
- * @version 0.1
- * @date 2020-03-31
- * @copyright Copyright (c) 2020
- */
+* @file PersonnelSchedule.h
+* @author Adam Adkins
+* @brief Defines the daily schedule of appointments for a specific Personnel
+* @version 0.1
+* @date 2020-03-31
+* @copyright Copyright (c) 2020
+*/
 
 #ifndef PERSONNELSCHEDULE_H
 #define PERSONNELSCHEDULE_H
@@ -19,46 +19,46 @@
 
 using namespace std;
 
-class PersonnelShedule : public Schedule {
+class PersonnelSchedule : public Schedule {
 
 
-    private: 
-        Personnel personnel;
+private:
+	Personnel personnel;
 
-    public:
-        /**
-         * @brief Construct a new Personel Shedule object
-         * 
-         */
-        PersonnelShedule();
-        /**
-         * @brief Construct a new Personel Shedule object
-         * 
-         * @param personnel 
-         * @param date 
-         * @param appointments 
-         * @param AvailableSlots 
-         */
-        PersonnelShedule(Personnel personnel, Date date, Appointment appointments, AppointmentSlot AvailableSlots);
-        /**
-         * @brief Destroy the Personel Shedule object
-         * 
-         */
-        ~PersonnelShedule();
+public:
+	/**
+	* @brief Construct a new Personel Shedule object
+	*
+	*/
+	PersonnelSchedule();
+	/**
+	* @brief Construct a new Personel Shedule object
+	*
+	* @param personnel
+	* @param date
+	* @param appointments
+	* @param AvailableSlots
+	*/
+	PersonnelSchedule(Personnel p, Date d, vector<Appointment> a, vector<AppointmentSlot> as);
+	/**
+	* @brief Destroy the Personel Shedule object
+	*
+	*/
+	~PersonnelSchedule();
 
-        /**
-         * @brief Get the Personnel object
-         * 
-         * @return Personnel 
-         */
-        Personnel getPersonnel();
+	/**
+	* @brief Get the Personnel object
+	*
+	* @return Personnel
+	*/
+	Personnel getPersonnel();
 
-        /**
-         * @brief Set the Personnel object
-         * 
-         * @param personnel 
-         */
-        void setPersonnel(Personnel personnel);
+	/**
+	* @brief Set the Personnel object
+	*
+	* @param personnel
+	*/
+	void setPersonnel(Personnel p);
 };
 
 #endif
